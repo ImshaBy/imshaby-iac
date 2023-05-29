deb52a2a4c1c757520dc744e325219ed9f36f092
Andrei Misan <andrei.misan@gmail.com>
2023-05-29 15:28:38 +0300

    remove personal

--- tf/01 - providers.tf
+++ tf/01 - providers.tf
@@ -4,18 +4,7 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "tf-bucket-imshaby"
-    region     = "ru-central1"
-    key        = "production/common/terraform.tfstate"
-    access_key = "YCAJEs4Mpc5HyfSn2j9VIt3Mb"
-    secret_key = "YCPxjsAi8a8o1Q33r9Lyg2TFy4-m7Ggxj0iT-jlU"
-    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g9o0e73jl7pt139g3j/etn0fqbjochuf9pn3upv"
-    dynamodb_table    = "state-lock-table"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
+
   required_version = ">= 0.13"
 }
 
